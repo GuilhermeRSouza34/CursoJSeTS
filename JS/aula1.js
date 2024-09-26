841ef5f9a873ef274d20aa7a2878c05eb261de8e
Guilherme <guilhermersouza34@gmail.com>
2024-09-26 18:47:03 -0300

    Aprendendo_console.log

---
+++ JS/aula1.js
@@ -0,0 +1,3 @@
+console.log("Guilherme");
+console.log(10, 6, 2003, "Desenvolvedor full stack");
+console.log('meu nome é "Guilherme" esse e o curso de js e estou estudando as', 17, 'da tarde'); //Primeiro Desafio curso de JS
