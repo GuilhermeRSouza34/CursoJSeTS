45e3bbf744688c16766edb46565e87abc05b607f
Guilherme <guilhermersouza34@gmail.com>
2024-09-26 19:38:52 -0300

    Curso completo JS e TS

---
+++ JS/aula4/aula4.js
@@ -0,0 +1,4 @@
+console.log('João nasceu em 1984');
+console.log('Em 2000 João conheceu maria');
+console.log('Maria Teve 1 filho com João em 2015');
+console.log('O filho de João se chama Eduardo');
