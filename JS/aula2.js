bf97cf6dbc52f358e92863fb06c0394ff5a92e35
Guilherme <guilhermersouza34@gmail.com>
2024-09-26 18:55:35 -0300

    Aula2

---
+++ JS/aula2.js
@@ -0,0 +1,12 @@
+//Aprendendo a utilizar os comentarios
+console.log('Olá Mundo!'); //outro comentario
+console.log('Linha1');
+console.log('Linha2');
+console.log('Linha3');
+console.log('Linha4');
+//vai ser ignorado
+
+/*
+ * comentario longo se utiliza assim
+ *
+ */
