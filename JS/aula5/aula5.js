b6a906e03b4f8558a6474981e22a0223ba6a3d7e
Guilherme <guilhermersouza34@gmail.com>
2024-09-27 21:09:11 -0300

    Curso completo JS e TS

--- JS/aula5/aula5.js
+++ JS/aula5/aula5.js
@@ -1,2 +1,11 @@
 const nome = 'João';
 console.log(nome);
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const conta = primeiroNumero * primeiroNumero;
+const conta2 = primeiroNumero + primeiroNumero;
+const conta3 = primeiroNumero - primeiroNumero;
+console.log(conta);
+console.log(conta2);
+console.log(conta3);
