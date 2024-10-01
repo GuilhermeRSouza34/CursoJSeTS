79daf061cd3b647899efd2523b2acea7679412a0
Guilherme <guilhermersouza34@gmail.com>
2024-10-01 15:56:07 -0300

    Aula 8 - Operadores de Atribuição

--- JS/aula8/aula8.js
+++ JS/aula8/aula8.js
@@ -2,8 +2,25 @@
 *Aritiméticos
 * + Soma / Concatenação
 * - Subtração / * Multiplicação / ** Pontenciação
+* % Resto da divisão
 */
 
-const num1 = 1;
-const num2 = 2;
-console.log(num1 + num2);
+const num1 = 5;
+const num2 = 7;
+const num3 = 15;
+console.log((num1 + num2) * num3); //Tudo oque estiver dentro dos parenteses vai ser a Primeira Ordem depois potenciação, multiplicação e divisao e assim vai
+
+/*
+* Operadores de Incremento
+* let contador = 1;
+* contador++;
+* console.log(contador);
+* Aqui seria outra maneira de usar o contador
+
+* outra maneira de usar o Incremento quando se quer somar mais que um
+* const passo = 2;
+* let contador = 1;
+* contador = contador + passo;
+* console.log(contador);
+* sendo que toda vez que chamar ele ele vai somando de 2 em 2
+*/
