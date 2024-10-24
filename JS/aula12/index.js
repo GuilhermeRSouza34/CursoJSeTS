3c20ac155c449c83a62bf292709dfb8c323a140f
Guilherme <guilhermersouza34@gmail.com>
2024-10-24 15:30:22 -0300

    Aula 12 - Operadores de comparação

---
+++ JS/aula12/index.js
@@ -0,0 +1,14 @@
+const hora = 9;
+
+//if pode ser usado sozinho
+
+
+if (hora < 12) {
+    console.log('Bom dia!');
+}
+else if (hora <= 18) {
+    console.log('Boa Tarde!');
+}
+else {
+    console.log('Boa Noite!');
+}
