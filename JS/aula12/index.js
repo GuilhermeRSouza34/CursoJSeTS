67771cb4b6ae1547bbb8197ee6e08d361311f929
Guilherme <guilhermersouza34@gmail.com>
2024-10-24 15:35:12 -0300

    Aula 12 - Operadores de comparação

--- JS/aula12/index.js
+++ JS/aula12/index.js
@@ -1,7 +1,9 @@
 const hora = 9;
 
 //if pode ser usado sozinho
-
+// Sempre que utilizado o else, preciso de um if antes
+// Eu posso ter varios else ifs na checagem
+// poder ter apenas um else na checagem
 
 if (hora < 12) {
     console.log('Bom dia!');
