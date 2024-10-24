7125b74899caa9eee377c7bb69b3366fbe99e50f
Guilherme <guilhermersouza34@gmail.com>
2024-10-24 12:15:51 -0300

    Aula 10 - Operadores de comparação

---
+++ JS/aula10/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+> maior que
+< menor que
+>= maior ou igual a
+<= menor ou igual a
+== igualdade (valor)
+=== igualdade extrita
+!= diferente
+!== diferente extrito
+*/
+
+console.log(5 > 3); //true
+console.log(5 < 3); //false
+console.log(5 >= 5); //true
+console.log(5 <= 5); //true
+console.log(5 == 5); //true
