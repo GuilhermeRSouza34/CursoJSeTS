6896248e63382f078b18dd2f3492fb96b00871ef
Guilherme <guilhermersouza34@gmail.com>
2024-10-24 12:26:42 -0300

    Aula 10 - Operadores de comparação

--- JS/aula10/index.js
+++ JS/aula10/index.js
@@ -16,7 +16,7 @@ console.log(5 >= 5); //true
 console.log(5 <= 5); //true
 console.log(5 == 5); //true
 
-const comp = 10 > 5;
+const comp1 = 10 > 5;
 console.log(comp); //true
 
 const comp2 = 10 < 5;
@@ -27,3 +27,8 @@ console.log(comp3); //false
 
 const comp4 = 10 >= 5;
 console.log(comp4); //true
+
+const num1 = 10;
+const num2 = '10';
+const comp = num1 == num2;
+console.log(comp); //true
